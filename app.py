a1172349876e6a3527e5c6941e96aaa4595641ee
Swati Kumari <swati0811kumari@gmail.com>
2025-06-27 21:16:52 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
-import mysql.connector
 from datetime import timedelta
 
 # Initialize Flask app
@@ -62,18 +61,6 @@ def register():
 @app.route('/home')
 def home():
     return render_template('home.html')
-
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS contacts (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(100),
-        email VARCHAR(100),
-        phone VARCHAR(15),
-        message TEXT,
-        submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-""")
-db.commit()
 
 
 @app.route('/contact', methods=['GET', 'POST'])
